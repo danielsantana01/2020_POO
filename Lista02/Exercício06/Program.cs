21c900fceacbf8ba28568145a006c5bbf007231b
Daniel Amaral de Santana <daniel.amaral@academico.ifrn.edu.br>
2020-02-28 18:03:14 -0300

    Exercícios 05 a 07

--- Lista02/Exercício06/Program.cs
+++ Lista02/Exercício06/Program.cs
@@ -10,6 +10,20 @@ namespace Exercício06
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Digite três valores inteiros");
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+            int maior = 0;
+            int menor = 0;
+            if (n1 > n2 && n1 > n3) maior = n1;
+            if (n2 > n1 && n2 > n3) maior = n2;
+            if (n3 > n1 && n3 > n2) maior = n3;
+            if (n1 < n2 && n1 < n3) menor = n1;
+            if (n2 < n3 && n2 < n1) menor = n2;
+            if (n3 < n1 && n3 < n2) menor = n3;
+            int s = maior + menor;
+            Console.WriteLine($"A soma do maior com o menor número é {s}");
         }
     }
 }
--- Lista02/Exercício07/Program.cs
+++ Lista02/Exercício07/Program.cs
@@ -10,6 +10,14 @@ namespace Exercício07
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Digite os coeficientes a, b, e c de uma equação do II grau");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            double x1 = (-b + Math.Sqrt(Math.Pow(b, 2) - 4 * a * c)) / 2;
+            double x2 = (-b - Math.Sqrt(Math.Pow(b, 2) - 4 * a * c)) / 2;
+            Console.WriteLine($"As raízes são {x1} e {x2}");
+            Console.ReadKey();
         }
     }
 }

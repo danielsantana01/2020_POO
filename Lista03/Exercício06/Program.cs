40681353f556b2b3fc8c08da8d171f7c4f62a594
Daniel Amaral de Santana <daniel.amaral@academico.ifrn.edu.br>
2020-03-13 11:09:05 -0300

    Exercício 06

--- Lista03/Exercício06/Program.cs
+++ Lista03/Exercício06/Program.cs
@@ -10,19 +10,22 @@ namespace Exercício06
     {
         static void Main(string[] args)
         {
-            string maior;
+            string maior = " ";
             Console.WriteLine("Informe o primeiro horário");
             string h1 = Console.ReadLine();
-            string[] l1 = h1.Split();
+            string[] l1 = h1.Split(' ', ':');
             Console.WriteLine("Informe o segundo horário");
             string h2 = Console.ReadLine();
-            string[] l2 = Console.ReadLine().Split();
-            if (l1[1].Equals("AM") && l2[1].Equals("PM")) maior = h2;
-            else if (l1[1].Equals("PM") && l2[1].Equals("AM")) maior = h1;
+            string[] l2 = h2.Split(' ', ':');
+            if (l1[2].Equals("AM") && l2[2].Equals("PM")) maior = h2;
+            else if (l1[2].Equals("PM") && l2[2].Equals("AM")) maior = h1;
             else
             {
-               
+                if (double.Parse(l1[0]) + double.Parse(l1[1]) / 60 > double.Parse(l2[0]) + double.Parse(l2[1]) / 60) maior = h1;
+                else if (double.Parse(l1[0]) + double.Parse(l1[1]) / 60 < double.Parse(l2[0]) + double.Parse(l2[1]) / 60) maior = h2;
             }
+            Console.WriteLine($"O maior horário é {maior}");
+            Console.ReadKey();
         }
     }
 }

4f41c4a20b60aaec9916e6a31d2359a1d18e30e0
Daniel Amaral de Santana <daniel.amaral@academico.ifrn.edu.br>
2020-05-25 22:23:14 -0300

    Exercício 10

--- Lista04/Exercício10/Program.cs
+++ Lista04/Exercício10/Program.cs
@@ -11,10 +11,15 @@ namespace Exercício10
         static void Main(string[] args)
         {
             Console.WriteLine("Informe o valor unitário do produto");
-            double valor = double.Parse(Console.ReadLine());
+            double V = double.Parse(Console.ReadLine());
             Console.WriteLine("Informe a quantidade comprada");
-            int qntd = int.Parse(Console.ReadLine());
-            double total;
+            int Q = int.Parse(Console.ReadLine());
+            double total = 0;
+            if (Q > 0 && Q <= 10) total = Q * V * 0.95;
+            else if (Q >= 11 && Q <= 50) total = 9.5 * V + (Q - 10) * V * 0.9;
+            else if (Q > 50) total = 45.5 * V + (Q - 50) * V * 0.8;
+            Console.WriteLine($"O total a ser pago é R$ {total:0.00}");
+            Console.ReadKey();
         }
     }
 }
